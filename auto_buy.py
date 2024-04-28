162ea079c0f185103d21de0b180a06b0a685f02c
ethancic <cic.ethan@gmail.com>
2024-04-29 03:07:04 +0800

    update: auto_buy

--- auto_buy.py
+++ auto_buy.py
@@ -1,11 +1,11 @@
 from sell_strategies import *
 from squid_cheat import *
 
-buy_sqd_continous_commands = buy_sqd_continous(
-    order_number = 1000, 
-    ton_min = 15, 
-    ton_max = 60, 
-    ratio=0.9, 
-    ton_threshold=1)
+buy_sqd_continous_commands = buy_sell_sqd_continuous(
+    order_number = 1000,
+    buy_ratio=0.5,
+    ton_min = 5, 
+    ton_max = 15, 
+    ratio=0.5)
 
 execute_commands(buy_sqd_continous_commands, N_min=1, N_max=1, sleep_min=0, sleep_max=60)
